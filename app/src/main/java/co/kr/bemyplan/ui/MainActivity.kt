abe37042691203c6b4c87bdd36151435bfd43b4c
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-05 14:50:27 +0900

    [CHORE] #1 - data / ui / util 초기 폴더링 setting

--- app/src/main/java/co/kr/bemyplan/ui/MainActivity.kt
+++ app/src/main/java/co/kr/bemyplan/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package co.kr.bemyplan
+package co.kr.bemyplan.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.kr.bemyplan.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

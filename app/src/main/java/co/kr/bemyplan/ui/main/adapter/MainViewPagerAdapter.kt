e2a2e04b74e6e9de8c6467c629601a8589853cf7
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-05 22:12:36 +0900

    [FEAT] #3 - MainActivity BottomNavigationView 구현

---
+++ app/src/main/java/co/kr/bemyplan/ui/main/adapter/MainViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package co.kr.bemyplan.ui.main.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
+    val fragments = mutableListOf<Fragment>()
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}

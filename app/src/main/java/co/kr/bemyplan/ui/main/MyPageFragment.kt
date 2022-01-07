3a94bbb6fd25626e21eb6dd47d00316419e578fd
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-08 00:31:13 +0900

    [CHORE] #6 - Fragment명 변경

--- app/src/main/java/co/kr/bemyplan/ui/main/MyPageFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/MyPageFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import co.kr.bemyplan.R
 
-class ThirdFragment : Fragment() {
+class MyPageFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 }
--- app/src/main/java/co/kr/bemyplan/ui/main/ScrapFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/ScrapFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import co.kr.bemyplan.R
 
-class FourthFragment : Fragment() {
+class ScrapFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fourth, container, false)
+        return inflater.inflate(R.layout.fragment_scrap, container, false)
     }
 }

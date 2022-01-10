4ec206c5fcb0410a9274eb1e18e4cd517e2d949c
jeonghoon <shb03323@naver.com>
2022-01-10 17:34:45 +0900

    [FEAT] #9 Databinding Activity에만 구현

--- app/src/main/java/co/kr/bemyplan/ui/purchase/after/AfterPurchaseActivity.kt
+++ app/src/main/java/co/kr/bemyplan/ui/purchase/after/AfterPurchaseActivity.kt
@@ -2,6 +2,9 @@ package co.kr.bemyplan.ui.purchase.after
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import co.kr.bemyplan.R
+import co.kr.bemyplan.data.Post
 import co.kr.bemyplan.databinding.ActivityAfterPurchaseBinding
 import net.daum.mf.map.api.MapView
 
@@ -10,11 +13,18 @@ class AfterPurchaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAfterPurchaseBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_after_purchase)
+
+        // data 객체 생성
+        binding.post = Post("감성을 느낄 수 있는 힐링여행", "thisisuzzwon", 4)
+
+        // Kakao Map 세팅
+        setMap()
+
         setContentView(binding.root)
     }
 
-    fun setMap() {
+    private fun setMap() {
         val mapView = MapView(this)
         binding.mapView.addView(mapView)
     }

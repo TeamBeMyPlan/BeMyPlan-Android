43561d366cb55cf0383973c14197e046e4e4dd48
hooni <shb03323@naver.com>
2022-01-11 17:28:47 +0900

    [FEAT] #17 databinding 구현, recyclerview item data 적재

--- app/src/main/java/co/kr/bemyplan/ui/purchase/after/adapter/DailyContentsAdapter.kt
+++ app/src/main/java/co/kr/bemyplan/ui/purchase/after/adapter/DailyContentsAdapter.kt
@@ -1,16 +1,20 @@
 package co.kr.bemyplan.ui.purchase.after.adapter
 
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import co.kr.bemyplan.data.DailyContents
 import co.kr.bemyplan.data.Spot
 import co.kr.bemyplan.databinding.DailyContentsListBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class DailyContentsAdapter: RecyclerView.Adapter<DailyContentsAdapter.DailyContentsViewHolder>() {
 
     private val dailyContentsList = mutableListOf<DailyContents>()
-    private val spotList = mutableListOf<Spot>()
+    val spotList = mutableListOf<Spot>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyContentsViewHolder {
         val binding = DailyContentsListBinding.inflate(
@@ -22,17 +26,22 @@ class DailyContentsAdapter: RecyclerView.Adapter<DailyContentsAdapter.DailyConte
     }
 
     override fun onBindViewHolder(holder: DailyContentsViewHolder, position: Int) {
-        holder.onBind(dailyContentsList[position])
+        holder.onBind(spotList[position])
     }
 
     override fun getItemCount() = dailyContentsList.size
 
     class DailyContentsViewHolder(private val binding: DailyContentsListBinding)
         : RecyclerView.ViewHolder(binding.root) {
-            fun onBind(data: DailyContents) {
-                binding.tvPlace.text = data.placeName[0]
-                binding.tvContext.text = data.context[0]
-                binding.tvAddress.text = data.address[0]
+            fun onBind(data: Spot) {
+                binding.tvPlace.text = data.placeName
+                binding.tvContext.text = data.context
+                binding.tvAddress.text = data.address
+                binding.tvMovingTime.text = data.duration.toString()
+                Glide.with(binding.root)
+                    .load(data.photo)
+                    .apply(RequestOptions.bitmapTransform(RoundedCorners(5)))
+                    .into(binding.ivPhoto)
             }
         }
 }

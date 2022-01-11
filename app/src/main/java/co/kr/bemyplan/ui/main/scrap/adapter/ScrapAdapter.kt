88fc1a50264d5b205a539dbfb2adf68cfca79be2
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-11 23:47:58 +0900

    [FEAT] #19 - ScrapFragment RecyclerView Adapter 구현

---
+++ app/src/main/java/co/kr/bemyplan/ui/main/scrap/adapter/ScrapAdapter.kt
@@ -0,0 +1,43 @@
+package co.kr.bemyplan.ui.main.scrap.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import co.kr.bemyplan.R
+import co.kr.bemyplan.data.main.scrap.ContentModel
+import co.kr.bemyplan.databinding.ItemScrapContentBinding
+
+class ScrapAdapter : RecyclerView.Adapter<ScrapAdapter.ScrapViewHolder>() {
+    var itemList: List<ContentModel> = listOf()
+
+    class ScrapViewHolder(val binding: ItemScrapContentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(contentModel: ContentModel) {
+            binding.model = contentModel
+            binding.ivPhoto.clipToOutline = true
+            binding.executePendingBindings()
+
+//            clickScrap()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScrapViewHolder {
+        val binding = DataBindingUtil.inflate<ItemScrapContentBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_scrap_content,
+            parent,
+            false
+        )
+        return ScrapViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ScrapViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+}

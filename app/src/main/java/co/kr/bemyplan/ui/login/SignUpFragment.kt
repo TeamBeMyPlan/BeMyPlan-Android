cd5355397b275d46a3b51b58f22ca5bcc581fc98
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-12 15:36:28 +0900

    [FEAT] #28 - SignUpFragment 생성

---
+++ app/src/main/java/co/kr/bemyplan/ui/login/SignUpFragment.kt
@@ -0,0 +1,18 @@
+package co.kr.bemyplan.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.kr.bemyplan.R
+
+class SignUpFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+}

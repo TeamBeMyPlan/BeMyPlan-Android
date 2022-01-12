fec7cda1ad2149eac20ce6b62ea597e82785bcf6
jeonghoon <shb03323@naver.com>
2022-01-12 16:37:05 +0900

    [FEAT] #26 button 구현

--- app/src/main/java/co/kr/bemyplan/data/DailyContents.kt
+++ app/src/main/java/co/kr/bemyplan/data/DailyContents.kt
@@ -1,10 +1,12 @@
 package co.kr.bemyplan.data
 
 data class DailyContents(
-    val placeName: List<String>,
-    val address: List<String>,
-    val image: List<String>,
-    val context: List<String>,
-    val transportation: List<String>,
-    val duration: List<Int>
+    val day: Int,
+    val isLast: Boolean,
+//    val placeName: List<String>,
+//    val address: List<String>,
+//    val image: List<String>,
+//    val context: List<String>,
+//    val transportation: List<String>,
+//    val duration: List<Int>
 )

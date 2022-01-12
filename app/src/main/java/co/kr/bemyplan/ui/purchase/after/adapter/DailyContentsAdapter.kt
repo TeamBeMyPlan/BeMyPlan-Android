ac890bb851df360865858e9debf470fd2b77c1d4
jeonghoon <shb03323@naver.com>
2022-01-12 13:06:32 +0900

    [FEAT] #17 하나의 adapter에 두개의 recyclerview 연결

--- app/src/main/java/co/kr/bemyplan/ui/purchase/after/adapter/DailyContentsAdapter.kt
+++ app/src/main/java/co/kr/bemyplan/ui/purchase/after/adapter/DailyContentsAdapter.kt
@@ -1,61 +1,97 @@
 package co.kr.bemyplan.ui.purchase.after.adapter
 
-import android.util.Log
+import android.annotation.SuppressLint
+import android.text.Layout
 import android.view.LayoutInflater
-import android.view.RoundedCorner
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableArrayList
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import co.kr.bemyplan.data.DailyContents
 import co.kr.bemyplan.data.Spot
 import co.kr.bemyplan.databinding.ItemDailyContentsBinding
+import co.kr.bemyplan.databinding.ItemDailyRouteBinding
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import java.lang.IllegalStateException
 
-class DailyContentsAdapter: RecyclerView.Adapter<DailyContentsAdapter.DailyContentsViewHolder>() {
+class DailyContentsAdapter(private val viewType: Int): RecyclerView.Adapter<DailyContentsAdapter.DailyContentsViewHolder>() {
 
-    private val dailyContentsList = mutableListOf<DailyContents>()
-    var spotList = listOf<Spot>()
+    private var spotList = listOf<Spot>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyContentsViewHolder {
-        Log.d("hellll","initAdapter")
-        val binding = ItemDailyContentsBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent, false
-        )
+    override fun getItemViewType(position: Int) = viewType
 
-        return DailyContentsViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when(viewType) {
+        TYPE_CONTENTS -> {
+            val binding = ItemDailyContentsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false
+            )
+            ContentsViewHolder(binding)
+        }
+        TYPE_ROUTE -> {
+            val binding = ItemDailyRouteBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent, false
+            )
+            RouteViewHolder(binding)
+        }
+        else -> {
+            throw IllegalStateException("Not Found ViewHolder Type $viewType")
+        }
     }
 
     override fun onBindViewHolder(holder: DailyContentsViewHolder, position: Int) {
-        holder.onBind(spotList[position])
+        when(holder) {
+            is ContentsViewHolder -> {
+                holder.onBind(spotList[position])
+            }
+            is RouteViewHolder -> {
+                holder.onBind(spotList[position])
+            }
+        }
     }
 
     override fun getItemCount() = spotList.size
 
-    class DailyContentsViewHolder(private val binding: ItemDailyContentsBinding)
-        : RecyclerView.ViewHolder(binding.root) {
-            fun onBind(data: Spot) {
-
-                binding.spot = data
-                Glide.with(binding.root)
-                    .load(data.photo)
-                    .apply(RequestOptions.bitmapTransform(RoundedCorners(5)))
-                    .into(binding.ivPhoto)
-            }
-        }
-
+    @SuppressLint("NotifyDataSetChanged")
     fun setItems(items: List<Spot>) {
-        Log.d("asdf","asdfasdf")
         spotList = items
         notifyDataSetChanged()
     }
 
-    @BindingAdapter("bind:spotList")
+    // MVVM을 위한 코드
+   @BindingAdapter("bind:spotList")
     fun bindSpotList(recyclerView: RecyclerView, items: ObservableArrayList<Spot>) {
         val adapter = recyclerView.adapter as? DailyContentsAdapter
         adapter?.setItems(items)
+    }
+
+    abstract class DailyContentsViewHolder(private val binding: ViewDataBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        abstract fun onBind(data: Spot)
+    }
+
+    class ContentsViewHolder(private val binding: ItemDailyContentsBinding): DailyContentsViewHolder(binding) {
+        override fun onBind(data: Spot) {
+            binding.spot = data
+            Glide.with(binding.root)
+                .load(data.photo)
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(5)))
+                .into(binding.ivPhoto)
+        }
+    }
+
+    class RouteViewHolder(private val binding: ItemDailyRouteBinding): DailyContentsViewHolder(binding) {
+        override fun onBind(data: Spot) {
+            binding.spot = data
+        }
+    }
+
+    companion object {
+        const val TYPE_CONTENTS = 0
+        const val TYPE_ROUTE = 1
     }
 }

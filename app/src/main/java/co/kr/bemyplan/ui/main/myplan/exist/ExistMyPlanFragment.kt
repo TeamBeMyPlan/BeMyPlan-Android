60849c82ced271054b8f66ef51915a9324659f4d
jeonghoon <shb03323@naver.com>
2022-01-13 14:13:22 +0900

    [FEAT] #35 - fragment_exist_my_plan 뷰 작업

---
+++ app/src/main/java/co/kr/bemyplan/ui/main/myplan/exist/ExistMyPlanFragment.kt
@@ -0,0 +1,18 @@
+package co.kr.bemyplan.ui.main.myplan.exist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.kr.bemyplan.R
+
+class ExistMyPlanFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exist_my_plan, container, false)
+    }
+}
---
+++ app/src/main/java/co/kr/bemyplan/ui/main/myplan/exist/adapter/ExistMyPlanAdapter.kt
@@ -0,0 +1,4 @@
+package co.kr.bemyplan.ui.main.myplan.exist.adapter
+
+class ExistMyPlanAdapter {
+}

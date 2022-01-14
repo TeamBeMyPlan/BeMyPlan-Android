c705f0ef5f406353aa7af42056c93fce3b9421b8
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-14 22:29:36 +0900

    [REFACTOR] #37 - 자동 줄 바꿈 및 미사용 import 제거

--- app/src/main/java/co/kr/bemyplan/util/CustomDialog.kt
+++ app/src/main/java/co/kr/bemyplan/util/CustomDialog.kt
@@ -6,23 +6,26 @@ import android.view.WindowManager
 import android.widget.TextView
 import androidx.annotation.LayoutRes
 import co.kr.bemyplan.R
-import org.w3c.dom.Text
 
 class CustomDialog(context: Context, val title: String, val content: String) {
     private val dialog = Dialog(context)
-    private lateinit var onClickedListener : ButtonClickListener
+    private lateinit var onClickedListener: ButtonClickListener
 
-    interface ButtonClickListener{
-        fun onClicked(num : Int)
+    interface ButtonClickListener {
+        fun onClicked(num: Int)
     }
-    fun setOnClickedListener(listener : ButtonClickListener){
+
+    fun setOnClickedListener(listener: ButtonClickListener) {
         onClickedListener = listener
     }
 
-    fun showDialog(@LayoutRes layout : Int){
+    fun showChoiceDialog(@LayoutRes layout: Int) {
         dialog.setContentView(layout)
-        dialog.findViewById<TextView>(R.id.tv_title).text = title
-        dialog.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.findViewById<TextView>(R.id.tv_title).text = content
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
         dialog.window?.setBackgroundDrawableResource(R.drawable.inset_horizontal_24)
         dialog.show()
 

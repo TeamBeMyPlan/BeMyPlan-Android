057605aab08d261b12aa3f40098bc66b658e67ef
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-14 22:29:05 +0900

    [FEAT] #37 - 확인 다이얼로그 show 함수 구현

--- app/src/main/java/co/kr/bemyplan/util/CustomDialog.kt
+++ app/src/main/java/co/kr/bemyplan/util/CustomDialog.kt
@@ -8,7 +8,7 @@ import androidx.annotation.LayoutRes
 import co.kr.bemyplan.R
 import org.w3c.dom.Text
 
-class CustomDialog(context: Context, val title: String) {
+class CustomDialog(context: Context, val title: String, val content: String) {
     private val dialog = Dialog(context)
     private lateinit var onClickedListener : ButtonClickListener
 
@@ -29,6 +29,23 @@ class CustomDialog(context: Context, val title: String) {
         dialog.findViewById<TextView>(R.id.tv_no).setOnClickListener {
             dialog.dismiss()
         }
+        dialog.findViewById<TextView>(R.id.tv_yes).setOnClickListener {
+            onClickedListener.onClicked(1)
+            dialog.dismiss()
+        }
+    }
+
+    fun showConfirmDialog(@LayoutRes layout: Int) {
+        dialog.setContentView(layout)
+        dialog.findViewById<TextView>(R.id.tv_title).text = title
+        dialog.findViewById<TextView>(R.id.tv_content).text = content
+        dialog.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(R.drawable.inset_horizontal_24)
+        dialog.show()
+
         dialog.findViewById<TextView>(R.id.tv_yes).setOnClickListener {
             onClickedListener.onClicked(1)
             dialog.dismiss()

a92cda475847ae4bab6c8964c1ab7ff1008b61af
jeonghoon <shb03323@naver.com>
2022-01-14 22:06:59 +0900

    [FEAT] #41 - SettingsActivity 구현

--- app/src/main/java/co/kr/bemyplan/ui/main/myplan/settings/SettingsActivity.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/myplan/settings/SettingsActivity.kt
@@ -11,5 +11,36 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 뒤로가기
+        binding.ivBack.setOnClickListener {
+            finish()
+        }
+
+        // 콘텐츠 업로드 신청
+        binding.ivUpload.setOnClickListener {
+            TODO()
+        }
+
+        // 문의하기
+        binding.ivQna.setOnClickListener {
+            TODO()
+        }
+
+        // 이용약관
+        binding.ivInfo.setOnClickListener {
+            TODO()
+        }
+
+        // 로그아웃
+        binding.tvLogout.setOnClickListener {
+            TODO()
+            finish()
+        }
+
+        // 회원탈퇴
+        binding.tvWithdrawal.setOnClickListener {
+            TODO()
+        }
     }
 }

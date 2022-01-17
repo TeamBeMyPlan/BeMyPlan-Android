7a7704407d927d8737173d821a501f3e97f47eef
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-17 17:45:41 +0900

    [REFACTOR] #53 - import 수정

--- app/src/main/java/co/kr/bemyplan/ui/purchase/before/BeforeChargingFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/purchase/before/BeforeChargingFragment.kt
@@ -4,12 +4,12 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.graphics.Rect
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.NestedScrollView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import co.kr.bemyplan.R
 import co.kr.bemyplan.data.purchase.before.ContentModel
 import co.kr.bemyplan.data.purchase.before.SummaryModel

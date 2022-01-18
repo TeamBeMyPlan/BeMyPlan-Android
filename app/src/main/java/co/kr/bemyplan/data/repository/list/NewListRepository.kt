bc807360f3f4def7d550baa2a978fdd86663bbea
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-18 17:21:17 +0900

    [REFACTOR] #72 - 서버 수정으로 인한 변동사항 적용

--- app/src/main/java/co/kr/bemyplan/data/repository/list/NewListRepository.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/list/NewListRepository.kt
@@ -3,5 +3,5 @@ package co.kr.bemyplan.data.repository.list
 import co.kr.bemyplan.data.entity.list.ResponseNewList
 
 interface NewListRepository {
-    suspend fun getNewList(page: Int): ResponseNewList
+    suspend fun getNewList(page: Int, pageSize: Int): ResponseNewList
 }

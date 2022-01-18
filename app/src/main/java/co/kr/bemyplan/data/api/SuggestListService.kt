b937b07f67c706ca47e506f661ef7a972fe493ca
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-18 17:21:44 +0900

    [REFACTOR] #72 - 서버 수정으로 인한 변동사항 적용

--- app/src/main/java/co/kr/bemyplan/data/api/SuggestListService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/SuggestListService.kt
@@ -7,6 +7,7 @@ import retrofit2.http.Query
 interface SuggestListService {
     @GET("api/v1/post/suggest/")
     suspend fun getSuggestList(
-        @Query("page") page: Int
+        @Query("page") page: Int,
+        @Query("pageSize") pageSize: Int
     ): ResponseSuggestList
 }

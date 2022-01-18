4336abce85e601003bf9d3b0710be7e574e26485
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-18 17:01:36 +0900

    [FEAT] #72 - 추천 여행 리스트 가져오는 getSuggestList() 구현

--- app/src/main/java/co/kr/bemyplan/ui/list/viewmodel/ListViewModel.kt
+++ app/src/main/java/co/kr/bemyplan/ui/list/viewmodel/ListViewModel.kt
@@ -7,19 +7,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.bemyplan.data.entity.list.ContentModel
 import co.kr.bemyplan.data.repository.list.NewListRepositoryImpl
+import co.kr.bemyplan.data.repository.list.SuggestListRepositoryImpl
 import kotlinx.coroutines.launch
 
 class ListViewModel: ViewModel() {
-    private val newListRepositoryImpl = NewListRepositoryImpl()
-
     private var _newList = MutableLiveData<List<ContentModel>>()
     val newList: LiveData<List<ContentModel>> get() = _newList
 
+    private var _suggestList = MutableLiveData<List<ContentModel>>()
+    val suggestList: LiveData<List<ContentModel>> get() = _suggestList
+
     fun getNewList(page: Int) {
+        val newListRepositoryImpl = NewListRepositoryImpl()
         viewModelScope.launch {
             val response = newListRepositoryImpl.getNewList(page)
             _newList.value = response.data
             Log.d("mlog: ListViewModel.newList.size", newList.value?.size.toString())
+        }
+    }
+
+    fun getSuggestList(page: Int) {
+        val suggestListRepositoryImpl = SuggestListRepositoryImpl()
+        viewModelScope.launch {
+            val response = suggestListRepositoryImpl.getSuggestList(page)
+            _suggestList.value = response.data
+            Log.d("mlog: ListViewModel.suggestList.size", suggestList.value?.size.toString())
         }
     }
 }

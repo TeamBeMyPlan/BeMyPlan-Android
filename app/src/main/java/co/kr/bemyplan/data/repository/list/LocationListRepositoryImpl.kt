dc3ddfb60948d52210a90a10864f7c76a5695c82
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-18 17:45:14 +0900

    [FEAT] #75 - RepositoryImpl 구현

---
+++ app/src/main/java/co/kr/bemyplan/data/repository/list/LocationListRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package co.kr.bemyplan.data.repository.list
+
+import co.kr.bemyplan.data.api.ApiService
+import co.kr.bemyplan.data.entity.list.ResponseLocationList
+
+class LocationListRepositoryImpl: LocationListRepository {
+    override suspend fun getLocationList(
+        area_id: Int,
+        page: Int,
+        pageSize: Int,
+        sort: String
+    ): ResponseLocationList {
+        return ApiService.locationListService.getLocationList(area_id, page, pageSize, sort)
+    }
+}

a2d766d5e136e95803887a66ff26d2edd514370a
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-18 17:21:41 +0900

    [REFACTOR] #72 - 서버 수정으로 인한 변동사항 적용

--- app/src/main/java/co/kr/bemyplan/data/repository/list/SuggestListRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/list/SuggestListRepositoryImpl.kt
@@ -4,7 +4,7 @@ import co.kr.bemyplan.data.api.ApiService
 import co.kr.bemyplan.data.entity.list.ResponseSuggestList
 
 class SuggestListRepositoryImpl: SuggestListRepository {
-    override suspend fun getSuggestList(page: Int): ResponseSuggestList {
-        return ApiService.suggestListService.getSuggestList(page)
+    override suspend fun getSuggestList(page: Int, pageSize: Int): ResponseSuggestList {
+        return ApiService.suggestListService.getSuggestList(page, pageSize)
     }
 }

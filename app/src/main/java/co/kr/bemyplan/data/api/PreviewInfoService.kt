922730f912cd22340d8e3ca3f2948f628a3bb1a7
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-19 09:24:45 +0900

    [FEAT] #82 - Retrofit Interface 구현

---
+++ app/src/main/java/co/kr/bemyplan/data/api/PreviewInfoService.kt
@@ -0,0 +1,12 @@
+package co.kr.bemyplan.data.api
+
+import co.kr.bemyplan.data.entity.purchase.before.ResponsePreviewInfo
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PreviewInfoService {
+    @GET("api/v1/post/{post_id}/preview/tag")
+    suspend fun getPreviewInfo(
+        @Path("post_id") post_id: Int
+    ): ResponsePreviewInfo
+}

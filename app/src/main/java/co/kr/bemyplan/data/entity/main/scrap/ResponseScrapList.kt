1312e553168f54519917b8090862b7d92cfb7142
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 01:43:40 +0900

    [FEAT] #114 - 서버 대공사

--- app/src/main/java/co/kr/bemyplan/data/entity/main/scrap/ResponseScrapList.kt
+++ app/src/main/java/co/kr/bemyplan/data/entity/main/scrap/ResponseScrapList.kt
@@ -3,5 +3,10 @@ package co.kr.bemyplan.data.entity.main.scrap
 import co.kr.bemyplan.data.entity.list.ContentModel
 
 data class ResponseScrapList(
-    val data: List<ContentModel>
-)
+    val data: Data
+) {
+    data class Data(
+        val items: List<ContentModel>,
+        val totalPage: Int
+    )
+}

cfdb8f3f12263017030aebd92c9ec93c591911cd
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 01:43:35 +0900

    [FEAT] #114 - 서버 대공사

--- app/src/main/java/co/kr/bemyplan/data/entity/main/myplan/ResponseMyPlan.kt
+++ app/src/main/java/co/kr/bemyplan/data/entity/main/myplan/ResponseMyPlan.kt
@@ -1,5 +1,11 @@
 package co.kr.bemyplan.data.entity.main.myplan
 
 data class ResponseMyPlan(
-    val data: List<MyModel>
-)
+    val data: Data
+) {
+    data class Data(
+        val items: List<MyModel>,
+        val totalCount: Int,
+        val totalPage: Int
+    )
+}

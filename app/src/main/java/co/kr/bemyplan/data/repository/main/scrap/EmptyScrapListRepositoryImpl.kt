db4cd07290f300d74a61755f08ad205eabba989b
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 01:44:46 +0900

    [FEAT] #114 - 서버 대공사

---
+++ app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/EmptyScrapListRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package co.kr.bemyplan.data.repository.main.scrap
+
+import co.kr.bemyplan.data.api.ApiService
+import co.kr.bemyplan.data.entity.main.scrap.ResponseEmptyScrapList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class EmptyScrapListRepositoryImpl: EmptyScrapListRepository {
+    override suspend fun getEmptyScrapList(): ResponseEmptyScrapList {
+        return withContext(Dispatchers.IO) {
+            ApiService.emptyScrapListService.getEmptyScrapList()
+        }
+    }
+}

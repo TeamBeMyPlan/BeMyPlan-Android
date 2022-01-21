be14214fb26419fb1209ffa62ac81579f9de47b9
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 13:39:46 +0900

    [FEAT] #125 - 소셜 로그인 statusCode 403 받아오기

--- app/src/main/java/co/kr/bemyplan/ui/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/co/kr/bemyplan/ui/login/viewmodel/LoginViewModel.kt
@@ -4,11 +4,32 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.kr.bemyplan.data.entity.login.RequestLogin
+import co.kr.bemyplan.data.entity.login.UserInfoModel
+import co.kr.bemyplan.data.repository.login.LoginRepositoryImpl
 import co.kr.bemyplan.util.SingleLiveEvent
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class LoginViewModel : ViewModel() {
     var nickname = MutableLiveData<String>("")
+
+    private var _socialToken = MutableLiveData<String>()
+    val socialToken: LiveData<String> get() = _socialToken
+
+    private var _socialType = MutableLiveData<String>()
+    val socialType: LiveData<String> get() = _socialType
+
+    private var _userInfo = MutableLiveData<UserInfoModel>()
+    val userInfo: LiveData<UserInfoModel> get() = _userInfo
+
+    private var _isUser = SingleLiveEvent<Boolean>()
+    val isUser: LiveData<Boolean> get() = _isUser
+
+//    private var _responseLogin = MutableLiveData<ResponseLogin>()
+//    val responseLogin: LiveData<ResponseLogin> get() = _responseLogin
 
     private var _isAllAgree = MutableLiveData<Boolean>(false)
     val isAllAgree: LiveData<Boolean> get() = _isAllAgree
@@ -39,8 +60,40 @@ class LoginViewModel : ViewModel() {
 
     // 카카오로그인
     private val userApiClient = UserApiClient.instance
-    private var _kakaoToken = MutableLiveData<String>()
-    val kakaoToken: LiveData<String> get() = _kakaoToken
+
+    fun setSocialToken(token: String) {
+        _socialToken.value = token
+        Log.d("mlog: LoginViewModel::socialToken.value", socialToken.value.toString())
+    }
+
+    fun setSocialType(type: String) {
+        _socialType.value = type
+        Log.d("mlog: LoginViewModel::socialType.value", socialType.value.toString())
+    }
+
+    fun postLogin() {
+        val requestLogin = RequestLogin(socialToken.value.toString(), socialType.value.toString())
+        val loginRepositoryImpl = LoginRepositoryImpl()
+        viewModelScope.launch {
+            try {
+                val response = loginRepositoryImpl.postLogin(requestLogin)
+                _userInfo.value = response.data.userInfo
+                _isUser.value = true
+            } catch (e: retrofit2.HttpException) {
+                e.printStackTrace()
+                Log.e("mlog: HttpException", e.code().toString())
+                if (e.code() == 403) {
+                    _isUser.value = false
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Log.e("mlog: IOException", e.message.toString())
+            } catch (t: Throwable) {
+                t.printStackTrace()
+                Log.e("mlog: Throwable", t.message.toString())
+            }
+        }
+    }
 
     fun setAllAgree() {
         Log.d("mlog: setAllAgree()", "executed")
@@ -75,7 +128,7 @@ class LoginViewModel : ViewModel() {
     }
 
     fun checkIsDuplicated() {
-        if(nickname.value.toString() == "test") {
+        if (nickname.value.toString() == "test") {
             // 중복인 경우
             _isDuplicated.value = true
         } else {
@@ -94,20 +147,9 @@ class LoginViewModel : ViewModel() {
         checkIsDuplicated()
 
         Log.d("mlog: isDuplicated.value", isDuplicated.value.toString())
-        if(!isDuplicated.value!! && isValid.value!!) {
+        if (!isDuplicated.value!! && isValid.value!!) {
             _signUpPermission.call()
         }
-    }
-
-    fun setKakaoToken(token: String) {
-        _kakaoToken.value = token
-        _loginType.value = KAKAO
-
-        // 임시코드
-        _isMember.value = false
-
-        Log.d("mlog: kakaoToken.value", kakaoToken.value.toString())
-        Log.d("mlog: loginType.value", loginType.value.toString())
     }
 
     companion object {

3a8d6273c82859bc21d13d92d6d9a710618d3371
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 17:33:32 +0900

    [FEAT] #142 - 2차 서버 대공사

--- app/src/main/java/co/kr/bemyplan/data/api/NewListService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/NewListService.kt
@@ -2,6 +2,7 @@ package co.kr.bemyplan.data.api
 
 import co.kr.bemyplan.data.entity.list.ResponseNewList
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NewListService {

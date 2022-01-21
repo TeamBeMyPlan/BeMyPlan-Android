e8e059aa231a025a6bfe6c2d430140451bacb370
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 17:34:03 +0900

    [FEAT] #142 - 2차 서버 대공사

--- app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/ScrapListRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/ScrapListRepositoryImpl.kt
@@ -7,14 +7,12 @@ import kotlinx.coroutines.withContext
 
 class ScrapListRepositoryImpl : ScrapListRepository {
     override suspend fun getScrapList(
-        user_id: String,
         page: Int,
         pageSize: Int,
         sort: String
     ): ResponseScrapList {
         return withContext(Dispatchers.IO) {
             ApiService.scrapListService.getScrapList(
-                user_id,
                 page,
                 pageSize,
                 sort

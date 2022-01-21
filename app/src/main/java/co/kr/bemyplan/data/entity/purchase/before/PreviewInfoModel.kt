d97fede106442fa0e0be58b32c21983095ee5266
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 17:33:40 +0900

    [FEAT] #142 - 2차 서버 대공사

--- app/src/main/java/co/kr/bemyplan/data/entity/purchase/before/PreviewInfoModel.kt
+++ app/src/main/java/co/kr/bemyplan/data/entity/purchase/before/PreviewInfoModel.kt
@@ -1,17 +1,28 @@
 package co.kr.bemyplan.data.entity.purchase.before
 
+import com.google.gson.annotations.SerializedName
+
 data class PreviewInfoModel(
-    val author_id: Int,
+    @SerializedName("author_id")
+    val authorId: Int,
     val author: String,
     val title: String,
     val price: Int,
     val description: String,
-    val tag_theme: String,
-    val tag_count_spot: Int,
-    val tag_count_day: Int,
-    val tag_count_restaurant: Int,
-    val tag_partner: String,
-    val tag_money: String,
-    val tag_mobility: String,
-    val tag_month: Int
+    @SerializedName("tag_theme")
+    val tagTheme: String,
+    @SerializedName("tag_count_spot")
+    val tagCountSpot: Int,
+    @SerializedName("tag_count_day")
+    val tagCountDay: Int,
+    @SerializedName("tag_count_restaurant")
+    val tagCountRestaurant: Int,
+    @SerializedName("tag_partner")
+    val tagPartner: String,
+    @SerializedName("tag_money")
+    val tagMoney: String,
+    @SerializedName("tag_mobility")
+    val tagMobility: String,
+    @SerializedName("tag_month")
+    val tagMonth: Int
 )

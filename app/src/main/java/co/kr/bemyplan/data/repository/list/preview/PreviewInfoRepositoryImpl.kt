8d3f846324a5956228bf2678f5387fc9341c05d3
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 14:41:03 +0900

    [FEAT] #135 - withContext(Dispatchers.IO) 적용

--- app/src/main/java/co/kr/bemyplan/data/repository/list/preview/PreviewInfoRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/list/preview/PreviewInfoRepositoryImpl.kt
@@ -2,9 +2,13 @@ package co.kr.bemyplan.data.repository.list.preview
 
 import co.kr.bemyplan.data.api.ApiService
 import co.kr.bemyplan.data.entity.purchase.before.ResponsePreviewInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class PreviewInfoRepositoryImpl: PreviewInfoRepository {
+class PreviewInfoRepositoryImpl : PreviewInfoRepository {
     override suspend fun getPreviewInfo(post_id: Int): ResponsePreviewInfo {
-        return ApiService.previewInfoService.getPreviewInfo(post_id)
+        return withContext(Dispatchers.IO) {
+            ApiService.previewInfoService.getPreviewInfo(post_id)
+        }
     }
 }

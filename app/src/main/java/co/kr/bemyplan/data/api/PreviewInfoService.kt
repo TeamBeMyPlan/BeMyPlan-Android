3ebf5c803c3c32dad1d2e610ee4a65bf98a133a7
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-21 17:33:49 +0900

    [FEAT] #142 - 2차 서버 대공사

--- app/src/main/java/co/kr/bemyplan/data/api/PreviewInfoService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/PreviewInfoService.kt
@@ -7,6 +7,6 @@ import retrofit2.http.Path
 interface PreviewInfoService {
     @GET("api/v1/post/{post_id}/preview/tag")
     suspend fun getPreviewInfo(
-        @Path("post_id") post_id: Int
+        @Path("post_id") postId: Int
     ): ResponsePreviewInfo
 }

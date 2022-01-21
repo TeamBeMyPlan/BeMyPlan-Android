d9ed1ca2c3854c459ecf2a906d76044b98a5a047
권용민 <briandr@naver.com>
2022-01-21 19:34:31 +0900

    [FEAT] #153 - splash

--- app/src/main/java/co/kr/bemyplan/ui/main/SplashActivity.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/SplashActivity.kt
@@ -1,5 +1,6 @@
 package co.kr.bemyplan.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -8,6 +9,7 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import co.kr.bemyplan.R
 import co.kr.bemyplan.databinding.ActivitySplashBinding
+import co.kr.bemyplan.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashBinding
@@ -15,15 +17,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val fadeAnim : Animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation)
-            binding.imageView.startAnimation(fadeAnim)
-        }, 1000L)
+        val display = this.resources.displayMetrics
+        val deviceWidth = display.widthPixels
+        val ratio : Double = 100/360.0
+        val imagePadding : Int = (ratio * deviceWidth).toInt()
+        binding.imageView.setPadding(imagePadding, 0, imagePadding, 0)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val fadeAnim : Animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation)
-            binding.imageView2.startAnimation(fadeAnim)
-        }, 1000L)
+            binding.imageView.startAnimation(fadeAnim)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500L)
 
         setContentView(binding.root)
     }

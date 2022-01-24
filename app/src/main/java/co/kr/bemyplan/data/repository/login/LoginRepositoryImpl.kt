44a81a5b5cc4e93ed3935f66845253c56ebf1882
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-24 16:11:33 +0900

    [FEAT] #132 - 닉네임 중복검사 함수 구현

--- app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package co.kr.bemyplan.data.repository.login
 
-import android.util.Log
 import co.kr.bemyplan.data.api.ApiService
 import co.kr.bemyplan.data.entity.login.RequestLogin
 import co.kr.bemyplan.data.entity.login.ResponseLogin
+import co.kr.bemyplan.data.entity.login.check.RequestDuplicatedNickname
+import co.kr.bemyplan.data.entity.login.check.ResponseDuplicatedNickname
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -11,6 +12,12 @@ class LoginRepositoryImpl : LoginRepository {
     override suspend fun postLogin(requestLogin: RequestLogin): ResponseLogin {
         return withContext(Dispatchers.IO) {
             ApiService.loginService.postLogin(requestLogin)
+        }
+    }
+
+    override suspend fun postDuplicatedNickname(requestDuplicatedNickname: RequestDuplicatedNickname): ResponseDuplicatedNickname {
+        return withContext(Dispatchers.IO) {
+            ApiService.loginService.postDuplicatedNickname(requestDuplicatedNickname)
         }
     }
 }

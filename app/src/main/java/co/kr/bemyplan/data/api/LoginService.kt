e6aee004d700bfc7d5b87876186ce8da4187efa3
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-24 16:11:27 +0900

    [FEAT] #132 - 닉네임 중복검사 함수 구현

--- app/src/main/java/co/kr/bemyplan/data/api/LoginService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/LoginService.kt
@@ -2,6 +2,8 @@ package co.kr.bemyplan.data.api
 
 import co.kr.bemyplan.data.entity.login.RequestLogin
 import co.kr.bemyplan.data.entity.login.ResponseLogin
+import co.kr.bemyplan.data.entity.login.check.RequestDuplicatedNickname
+import co.kr.bemyplan.data.entity.login.check.ResponseDuplicatedNickname
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -10,4 +12,9 @@ interface LoginService {
     suspend fun postLogin(
         @Body body: RequestLogin
     ): ResponseLogin
+
+    @POST("api/v1/auth/check/nickname")
+    suspend fun postDuplicatedNickname(
+        @Body body: RequestDuplicatedNickname
+    ): ResponseDuplicatedNickname
 }

0a5ff88609ff8a2230f1c1e83203ae8e6f151b78
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-24 22:50:21 +0900

    [FEAT] #172 - RepositoryImpl 구현

--- app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepositoryImpl.kt
@@ -1,10 +1,12 @@
 package co.kr.bemyplan.data.repository.login
 
 import co.kr.bemyplan.data.api.ApiService
-import co.kr.bemyplan.data.entity.login.RequestLogin
-import co.kr.bemyplan.data.entity.login.ResponseLogin
+import co.kr.bemyplan.data.entity.login.login.RequestLogin
+import co.kr.bemyplan.data.entity.login.login.ResponseLogin
 import co.kr.bemyplan.data.entity.login.check.RequestDuplicatedNickname
 import co.kr.bemyplan.data.entity.login.check.ResponseDuplicatedNickname
+import co.kr.bemyplan.data.entity.login.signup.RequestSignUp
+import co.kr.bemyplan.data.entity.login.signup.ResponseSignUp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -18,6 +20,12 @@ class LoginRepositoryImpl : LoginRepository {
     override suspend fun postDuplicatedNickname(requestDuplicatedNickname: RequestDuplicatedNickname): ResponseDuplicatedNickname {
         return withContext(Dispatchers.IO) {
             ApiService.loginService.postDuplicatedNickname(requestDuplicatedNickname)
+        }
+    }
+
+    override suspend fun postSignUp(requestSignUp: RequestSignUp): ResponseSignUp {
+        return withContext(Dispatchers.IO) {
+            ApiService.loginService.postSignUp(requestSignUp)
         }
     }
 }

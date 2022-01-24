4043d347bb87d0b98ddcf81a06ef0bc9cc60630b
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-24 22:50:12 +0900

    [FEAT] #172 - Repository 구현

--- app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepository.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepository.kt
@@ -1,11 +1,14 @@
 package co.kr.bemyplan.data.repository.login
 
-import co.kr.bemyplan.data.entity.login.RequestLogin
-import co.kr.bemyplan.data.entity.login.ResponseLogin
+import co.kr.bemyplan.data.entity.login.login.RequestLogin
+import co.kr.bemyplan.data.entity.login.login.ResponseLogin
 import co.kr.bemyplan.data.entity.login.check.RequestDuplicatedNickname
 import co.kr.bemyplan.data.entity.login.check.ResponseDuplicatedNickname
+import co.kr.bemyplan.data.entity.login.signup.RequestSignUp
+import co.kr.bemyplan.data.entity.login.signup.ResponseSignUp
 
 interface LoginRepository {
     suspend fun postLogin(requestLogin: RequestLogin): ResponseLogin
     suspend fun postDuplicatedNickname(requestDuplicatedNickname: RequestDuplicatedNickname): ResponseDuplicatedNickname
+    suspend fun postSignUp(requestSignUp: RequestSignUp): ResponseSignUp
 }

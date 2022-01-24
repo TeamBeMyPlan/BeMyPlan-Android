267d290056c59f231ced828eeecd7cd32746f4b0
Seunghyeon Han <81508084+hansh0101@users.noreply.github.com>
2022-01-24 16:11:36 +0900

    [FEAT] #132 - 닉네임 중복검사 함수 선언

--- app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepository.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/login/LoginRepository.kt
@@ -2,7 +2,10 @@ package co.kr.bemyplan.data.repository.login
 
 import co.kr.bemyplan.data.entity.login.RequestLogin
 import co.kr.bemyplan.data.entity.login.ResponseLogin
+import co.kr.bemyplan.data.entity.login.check.RequestDuplicatedNickname
+import co.kr.bemyplan.data.entity.login.check.ResponseDuplicatedNickname
 
 interface LoginRepository {
     suspend fun postLogin(requestLogin: RequestLogin): ResponseLogin
+    suspend fun postDuplicatedNickname(requestDuplicatedNickname: RequestDuplicatedNickname): ResponseDuplicatedNickname
 }

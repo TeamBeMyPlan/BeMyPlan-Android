135f67ec8807163f26e51f5878dac902c5a490da
Seunghyeon Han <hansh0101@naver.com>
2022-03-21 16:46:56 +0900

    #240 [REFACTOR] : 코드 정리

--- app/src/main/java/co/kr/bemyplan/ui/login/signup/CheckEmailFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/login/signup/CheckEmailFragment.kt
@@ -3,14 +3,13 @@ package co.kr.bemyplan.ui.login.signup
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import co.kr.bemyplan.R
 import co.kr.bemyplan.databinding.FragmentCheckEmailBinding
@@ -27,7 +26,8 @@ class CheckEmailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_check_email, container, false)
+        _binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_check_email, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root

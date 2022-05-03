3893c080b236e609b2ff7a5b8243181348794b55
hansh0101 <hansh0101@naver.com>
2022-05-03 10:17:49 +0900

    [REFACTOR] #249 - Remove Unnecessary Codes

--- app/src/main/java/co/kr/bemyplan/ui/main/SplashActivity.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/SplashActivity.kt
@@ -69,22 +69,10 @@ class SplashActivity : AppCompatActivity() {
         if(dataStore.sessionId != "") {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            finish()
         } else {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-            finish()
         }
-
-//        TODO: Test 후 삭제요망
-//        if(AutoLoginData.getAutoLogin(this)) {
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        } else {
-//            val intent = Intent(this, LoginActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }
+        finish()
     }
 }

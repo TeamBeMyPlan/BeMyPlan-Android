6f0e8254229a11d4ef7e39d1dff8e3f670f5f353
hansh0101 <hansh0101@naver.com>
2022-05-04 09:28:40 +0900

    [REFACTOR] #251 - Reformat Code & Remove Unused Import

--- app/src/main/java/co/kr/bemyplan/data/api/LocationListService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/LocationListService.kt
@@ -1,9 +1,7 @@
 package co.kr.bemyplan.data.api
 
 import co.kr.bemyplan.data.entity.list.ResponseLocationList
-import com.google.gson.annotations.SerializedName
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface LocationListService {

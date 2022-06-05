862e19c94be3c1e2816dc27f8d0676ef24b8c058
hansh0101 <hansh0101@naver.com>
2022-06-05 17:49:14 +0900

    [FEAT] #269 - signOut 메서드 추가

--- app/src/main/java/co/kr/bemyplan/data/repository/logout/LogoutRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/logout/LogoutRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package co.kr.bemyplan.data.repository.logout
 
 import co.kr.bemyplan.data.api.LogoutService
+import co.kr.bemyplan.data.entity.logout.RequestSignOut
 import co.kr.bemyplan.domain.repository.LogoutRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -8,11 +9,17 @@ import javax.inject.Inject
 
 class LogoutRepositoryImpl @Inject constructor(
     private val logoutService: LogoutService,
-    private val coroutineDispatcher: CoroutineDispatcher
+    private val coroutineDispatcher: CoroutineDispatcher,
 ) : LogoutRepository {
     override suspend fun logout() {
         withContext(coroutineDispatcher) {
             logoutService.postLogout()
+        }
+    }
+
+    override suspend fun signOut(reason: String) {
+        withContext(coroutineDispatcher) {
+            logoutService.deleteSignOut(RequestSignOut(reason))
         }
     }
 }

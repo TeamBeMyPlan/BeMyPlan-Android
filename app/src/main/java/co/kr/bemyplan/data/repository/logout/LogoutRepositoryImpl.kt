a5ea45e0662c8e631ff5264f1bd071f7b34a3230
hansh0101 <hansh0101@naver.com>
2022-06-05 16:17:28 +0900

    [FEAT] #269 - LogoutRepositoryImpl 구현

---
+++ app/src/main/java/co/kr/bemyplan/data/repository/logout/LogoutRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package co.kr.bemyplan.data.repository.logout
+
+import co.kr.bemyplan.data.api.LogoutService
+import co.kr.bemyplan.domain.repository.LogoutRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class LogoutRepositoryImpl @Inject constructor(
+    private val logoutService: LogoutService,
+    private val coroutineDispatcher: CoroutineDispatcher
+) : LogoutRepository {
+    override suspend fun logout() {
+        withContext(coroutineDispatcher) {
+            logoutService.postLogout()
+        }
+    }
+}

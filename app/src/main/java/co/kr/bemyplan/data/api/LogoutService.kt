7bd15559fc6dbbaa285b2c8bcf1ed83059d22d85
hansh0101 <hansh0101@naver.com>
2022-06-05 17:49:19 +0900

    [FEAT] #269 - signOut 메서드 추가

--- app/src/main/java/co/kr/bemyplan/data/api/LogoutService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/LogoutService.kt
@@ -1,9 +1,18 @@
 package co.kr.bemyplan.data.api
 
+import co.kr.bemyplan.data.entity.logout.RequestSignOut
 import co.kr.bemyplan.data.entity.logout.ResponseLogout
+import co.kr.bemyplan.data.entity.logout.ResponseSignOut
+import retrofit2.http.Body
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 
 interface LogoutService {
     @POST("v1/logout")
     suspend fun postLogout(): ResponseLogout
+
+    @HTTP(method = "DELETE", path = "v1/signout", hasBody = true)
+    suspend fun deleteSignOut(
+        @Body requestBody: RequestSignOut
+    ): ResponseSignOut
 }

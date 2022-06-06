a55d26190b63dd5631c733d4df98f404883c3b7d
hansh0101 <hansh0101@naver.com>
2022-06-06 16:52:47 +0900

    [DEL] #280 - 미사용 import 제거

--- app/src/main/java/co/kr/bemyplan/ui/login/LoginFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/login/LoginFragment.kt
@@ -3,7 +3,6 @@ package co.kr.bemyplan.ui.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
--- app/src/main/java/co/kr/bemyplan/ui/login/SignUpFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/login/SignUpFragment.kt
@@ -6,13 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import co.kr.bemyplan.R
 import co.kr.bemyplan.databinding.FragmentSignUpBinding
 import co.kr.bemyplan.ui.login.signup.CheckNicknameFragment
-import co.kr.bemyplan.ui.login.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint

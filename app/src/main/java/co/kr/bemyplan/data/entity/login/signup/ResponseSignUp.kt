35f6b8c74453bd1a95b79f3b85af7f0ac2d6a39a
hansh0101 <hansh0101@naver.com>
2022-06-06 16:29:09 +0900

    [FEAT] #280 - Response 데이터 클래스 명세 수정

--- app/src/main/java/co/kr/bemyplan/data/entity/login/signup/ResponseSignUp.kt
+++ app/src/main/java/co/kr/bemyplan/data/entity/login/signup/ResponseSignUp.kt
@@ -1,6 +1,6 @@
 package co.kr.bemyplan.data.entity.login.signup
 
-import co.kr.bemyplan.data.entity.login.UserInfoModel
+import co.kr.bemyplan.domain.model.login.UserInfoModel
 
 data class ResponseSignUp(
     val data: UserInfoModel,

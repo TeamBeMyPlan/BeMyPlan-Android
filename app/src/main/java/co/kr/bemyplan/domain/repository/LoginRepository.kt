132924a964ee1991038761c8cf8f2240b46d60b3
hansh0101 <hansh0101@naver.com>
2022-06-06 16:25:56 +0900

    [DEL] #280 - 미사용 import 제거

--- app/src/main/java/co/kr/bemyplan/domain/repository/LoginRepository.kt
+++ app/src/main/java/co/kr/bemyplan/domain/repository/LoginRepository.kt
@@ -1,7 +1,5 @@
 package co.kr.bemyplan.domain.repository
 
-import co.kr.bemyplan.data.entity.login.login.RequestLogin
-import co.kr.bemyplan.data.entity.login.login.ResponseLogin
 import co.kr.bemyplan.data.entity.login.signup.RequestSignUp
 import co.kr.bemyplan.data.entity.login.signup.ResponseSignUp
 

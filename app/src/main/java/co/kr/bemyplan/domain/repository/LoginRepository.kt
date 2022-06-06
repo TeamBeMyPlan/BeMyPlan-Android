00f0853806fb9472c012f390b3f9b3598d6638bf
hansh0101 <hansh0101@naver.com>
2022-06-06 16:26:09 +0900

    [FEAT] #280 - 메서드 리턴타입 변경

--- app/src/main/java/co/kr/bemyplan/domain/repository/LoginRepository.kt
+++ app/src/main/java/co/kr/bemyplan/domain/repository/LoginRepository.kt
@@ -2,9 +2,10 @@ package co.kr.bemyplan.domain.repository
 
 import co.kr.bemyplan.data.entity.login.signup.RequestSignUp
 import co.kr.bemyplan.data.entity.login.signup.ResponseSignUp
+import co.kr.bemyplan.domain.model.login.UserInfoModel
 
 interface LoginRepository {
-    suspend fun postLogin(requestLogin: RequestLogin): ResponseLogin
+    suspend fun postLogin(socialType: String, token: String): UserInfoModel
     suspend fun postDuplicatedNickname(nickname: String): String
     suspend fun postSignUp(requestSignUp: RequestSignUp): ResponseSignUp
 }

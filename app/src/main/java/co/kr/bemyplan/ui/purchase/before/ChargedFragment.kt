559144ce7af280bbaf7a8d42be0251aebbdc350e
hansh0101 <hansh0101@naver.com>
2022-06-07 11:15:28 +0900

    [FEAT] #284 - 리턴 타입 not null 설정

--- app/src/main/java/co/kr/bemyplan/ui/purchase/before/ChargedFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/purchase/before/ChargedFragment.kt
@@ -19,7 +19,7 @@ class ChargedFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentChargedBinding.inflate(layoutInflater)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner

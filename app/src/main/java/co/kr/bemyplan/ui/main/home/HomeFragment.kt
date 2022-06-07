f796826ee2d43d559efd261b00d4d97626e250df
hansh0101 <hansh0101@naver.com>
2022-06-07 11:18:01 +0900

    [FEAT] #284 - 리턴 타입 not null 설정

--- app/src/main/java/co/kr/bemyplan/ui/main/home/HomeFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/home/HomeFragment.kt
@@ -29,7 +29,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
     }

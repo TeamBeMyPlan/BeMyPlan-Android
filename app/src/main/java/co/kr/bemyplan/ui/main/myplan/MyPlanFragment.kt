472a5d7b1c721699b016fc04159e0991c06c2bc0
hansh0101 <hansh0101@naver.com>
2022-06-20 15:02:02 +0900

    [feature] #299 - Firebase Analytics Log Event 추가

--- app/src/main/java/co/kr/bemyplan/ui/main/myplan/MyPlanFragment.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/myplan/MyPlanFragment.kt
@@ -2,17 +2,15 @@ package co.kr.bemyplan.ui.main.myplan
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import co.kr.bemyplan.R
+import co.kr.bemyplan.data.firebase.FirebaseAnalyticsProvider
 import co.kr.bemyplan.databinding.FragmentMyPlanBinding
 import co.kr.bemyplan.domain.model.main.myplan.MyPlanData
 import co.kr.bemyplan.ui.login.LoginActivity
@@ -21,7 +19,7 @@ import co.kr.bemyplan.ui.main.myplan.settings.SettingsActivity
 import co.kr.bemyplan.ui.main.myplan.viewmodel.MyPlanViewModel
 import co.kr.bemyplan.ui.purchase.after.AfterPurchaseActivity
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MyPlanFragment : Fragment() {
@@ -30,6 +28,9 @@ class MyPlanFragment : Fragment() {
     private val viewModel by viewModels<MyPlanViewModel>()
     private var listItem = listOf<MyPlanData.Data>()
     private lateinit var purchaseTourAdapter: MyPlanAdapter
+
+    @Inject
+    lateinit var firebaseAnalyticsProvider: FirebaseAnalyticsProvider
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -61,6 +62,10 @@ class MyPlanFragment : Fragment() {
     private fun initAdapter() {
         Log.d("asdf", "initAdapter 들어옴2")
         purchaseTourAdapter = MyPlanAdapter({
+            firebaseAnalyticsProvider.firebaseAnalytics.logEvent("clickTravelPlan", Bundle().apply {
+                putString("source", "마이플랜")
+                putInt("planId", it.planId)
+            })
             val intent = Intent(requireContext(), AfterPurchaseActivity::class.java)
             // 이 부분 작가 이름이랑 작가 아이디도 넘겨야하는데 어떻게 넘기면 좋을까...?
             // adapter에서 하던데 저 요소를 어떻게 추가해야할 지 모르겠어

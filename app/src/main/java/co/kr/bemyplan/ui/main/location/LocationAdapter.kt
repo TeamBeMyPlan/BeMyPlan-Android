76cdfe4013301bc6cb32cd793b9431a75b2ae07a
hansh0101 <hansh0101@naver.com>
2022-06-20 14:31:53 +0900

    [feature] #299 - Firebase Analytics Log Event 추가

--- app/src/main/java/co/kr/bemyplan/ui/main/location/LocationAdapter.kt
+++ app/src/main/java/co/kr/bemyplan/ui/main/location/LocationAdapter.kt
@@ -1,23 +1,28 @@
 package co.kr.bemyplan.ui.main.location
 
 import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import co.kr.bemyplan.data.firebase.FirebaseAnalyticsProvider
 import co.kr.bemyplan.databinding.ItemLocationBinding
 import co.kr.bemyplan.domain.model.main.location.LocationData
 import co.kr.bemyplan.util.ToastMessage.shortToast
 import co.kr.bemyplan.util.clipTo
+import javax.inject.Inject
 
-class LocationAdapter(val itemClick:(LocationData)->Unit, val myContext : Context) :
-    ListAdapter<LocationData, LocationAdapter.LocationViewHolder>(LocationComparator()){
+class LocationAdapter(val itemClick: (LocationData) -> Unit, val myContext: Context) :
+    ListAdapter<LocationData, LocationAdapter.LocationViewHolder>(LocationComparator()) {
+    @Inject
+    lateinit var firebaseAnalyticsProvider: FirebaseAnalyticsProvider
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding=ItemLocationBinding.inflate(layoutInflater, parent, false)
+        val binding = ItemLocationBinding.inflate(layoutInflater, parent, false)
         return LocationViewHolder(binding)
     }
 
@@ -25,28 +30,38 @@ class LocationAdapter(val itemClick:(LocationData)->Unit, val myContext : Contex
         holder.onBind(getItem(position))
     }
 
-    inner class LocationViewHolder(private val binding: ItemLocationBinding):RecyclerView.ViewHolder(binding.root) {
+    inner class LocationViewHolder(private val binding: ItemLocationBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: LocationData) {
             binding.locationItem = data
             clipTo(binding.ivLocation, data.thumbnailUrl)
             binding.root.setOnClickListener {
                 if (binding.ivLock.visibility == View.GONE) {
+                    firebaseAnalyticsProvider.firebaseAnalytics.logEvent(
+                        "clickOpenedTravelSpot",
+                        Bundle().apply {
+                            putString("spot", data.name)
+                        })
                     itemClick(data)
                 } else {
+                    firebaseAnalyticsProvider.firebaseAnalytics.logEvent(
+                        "clickClosedTravelSpot",
+                        Bundle().apply {
+                            putString("spot", data.name)
+                        })
                     myContext.shortToast("추후에 오픈될 예정입니다")
                 }
             }
         }
     }
 
-    private class LocationComparator:DiffUtil.ItemCallback<LocationData>(){
+    private class LocationComparator : DiffUtil.ItemCallback<LocationData>() {
         override fun areItemsTheSame(oldItem: LocationData, newItem: LocationData): Boolean {
-            return oldItem.region==newItem.region
+            return oldItem.region == newItem.region
         }
 
         override fun areContentsTheSame(oldItem: LocationData, newItem: LocationData): Boolean {
-            return oldItem==newItem
+            return oldItem == newItem
         }
-
     }
 }

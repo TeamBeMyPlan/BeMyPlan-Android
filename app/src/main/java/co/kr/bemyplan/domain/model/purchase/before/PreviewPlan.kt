d4133c06e342ae9995d90ab2fe0b0428b49e4fde
hansh0101 <hansh0101@naver.com>
2022-06-20 10:47:31 +0900

    [feature] #295 - 구매 전 여행일정 미리보기 정보 영문 -> 한글 변경

--- app/src/main/java/co/kr/bemyplan/domain/model/purchase/before/PreviewPlan.kt
+++ app/src/main/java/co/kr/bemyplan/domain/model/purchase/before/PreviewPlan.kt
@@ -13,6 +13,52 @@ data class PreviewPlan(
                     it.value = it.value.replace(" ", "")
                 }
             }
+
+            previewInfo.theme = Theme.koreanOf(previewInfo.theme)
+            previewInfo.partner = Partner.koreanOf(previewInfo.partner)
+            previewInfo.mobility = Mobility.koreanOf(previewInfo.mobility)
+        }
+    }
+
+    enum class Theme(val themeEnglish: String, val themeKorean: String) {
+        HEALING("HEALING", "힐링"),
+        EATING("EATING", "맛집"),
+        HOTPLACE("HOTPLACE", "핫플"),
+        LIFESHOT("LIFESHOT", "인생샷"),
+        LOCAL("LOCAL", "로컬"),
+        ACTIVITY("ACTIVITY", "액티비티"),
+        CAMPING("CAMPING", "캠핑");
+
+        companion object {
+            fun koreanOf(english: String): String {
+                return values().find { it.themeEnglish == english }?.themeKorean ?: throw IllegalArgumentException()
+            }
+        }
+    }
+
+    enum class Partner(val partnerEnglish: String, val partnerKorean: String) {
+        FAMILY("FAMILY", "가족"),
+        FRIEND("FRIEND", "친구"),
+        COUPLE("COUPLE", "연인"),
+        SOLO("SOLO", "혼자");
+
+        companion object {
+            fun koreanOf(english: String): String {
+                return values().find { it.partnerEnglish == english }?.partnerKorean ?: throw IllegalArgumentException()
+            }
+        }
+    }
+
+    enum class Mobility(val mobilityEnglish: String, val mobilityKorean: String) {
+        CAR("CAR", "승용차"),
+        PUBLIC("PUBLIC", "대중교통"),
+        WALK("WALK", "도보"),
+        BICYCLE("BICYCLE", "자전거");
+
+        companion object {
+            fun koreanOf(english: String): String {
+                return values().find {it.mobilityEnglish == english}?.mobilityKorean ?: throw IllegalArgumentException()
+            }
         }
     }
 }

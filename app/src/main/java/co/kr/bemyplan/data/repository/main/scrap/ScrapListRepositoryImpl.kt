d1c630ac73c287d7e4461cb7573302a143daaad2
hansh0101 <hansh0101@naver.com>
2022-06-20 22:56:06 +0900

    [feature] #302 - 페이징 구현

--- app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/ScrapListRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/ScrapListRepositoryImpl.kt
@@ -13,20 +13,39 @@ class ScrapListRepositoryImpl @Inject constructor(
     private val service: ScrapListService,
     private val coroutineDispatcher: CoroutineDispatcher
 ) : ScrapListRepository {
-    override suspend fun getScrapList(
+    override suspend fun fetchDefaultScrapList(): PlanList =
+        withContext(coroutineDispatcher) {
+            service.fetchDefaultScrapList(10).data
+        }
+
+    override suspend fun fetchQueryScrapList(
         sort: String
     ): PlanList {
         return withContext(coroutineDispatcher) {
-            service.getScrapList(
+            service.fetchQueryScrapList(
                 10,
                 sort
             ).data
         }
     }
 
-    override suspend fun getEmptyScrapList(): ResponseEmptyScrapList {
-        return withContext(Dispatchers.IO) {
+    override suspend fun fetchEmptyScrapList(): ResponseEmptyScrapList {
+        return withContext(coroutineDispatcher) {
             service.getEmptyScrapList()
         }
     }
+
+    override suspend fun fetchDefaultMoreScrapList(size: Int, lastScrapId: Int): PlanList =
+        withContext(coroutineDispatcher) {
+            service.fetchDefaultMoreScrapList(size, lastScrapId).data
+        }
+
+    override suspend fun fetchQueryMoreScrapList(
+        size: Int,
+        lastScrapId: Int,
+        sort: String
+    ): PlanList =
+        withContext(coroutineDispatcher) {
+            service.fetchQueryMoreScrapList(size, lastScrapId, sort).data
+        }
 }

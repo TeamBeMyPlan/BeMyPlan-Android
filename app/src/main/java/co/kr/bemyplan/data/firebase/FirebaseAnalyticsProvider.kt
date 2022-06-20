6875b42a885c7c467a1e3b978725d1a6eb47c137
hansh0101 <hansh0101@naver.com>
2022-06-20 17:53:42 +0900

    [feature] #299 - 생성자 주입으로 DI 코드 수정

--- app/src/main/java/co/kr/bemyplan/data/firebase/FirebaseAnalyticsProvider.kt
+++ app/src/main/java/co/kr/bemyplan/data/firebase/FirebaseAnalyticsProvider.kt
@@ -6,11 +6,10 @@ import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
-class FirebaseAnalyticsProvider() {
-    @Inject
-    lateinit var beMyPlanDataStore: BeMyPlanDataStore
-
-    val parameters = Bundle().apply {
+class FirebaseAnalyticsProvider @Inject constructor(
+    private val beMyPlanDataStore: BeMyPlanDataStore
+) {
+    private val parameters = Bundle().apply {
         this.putString("device", "Android")
         this.putInt("userId", beMyPlanDataStore.userId)
     }

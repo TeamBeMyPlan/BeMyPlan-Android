40f21b7e22e803dc8f4b7d9ef3ce472b15cdcf8d
hansh0101 <hansh0101@naver.com>
2022-06-20 22:56:12 +0900

    [feature] #302 - 페이징 구현

--- app/src/main/java/co/kr/bemyplan/data/api/ScrapListService.kt
+++ app/src/main/java/co/kr/bemyplan/data/api/ScrapListService.kt
@@ -3,16 +3,33 @@ package co.kr.bemyplan.data.api
 import co.kr.bemyplan.data.entity.main.scrap.ResponseEmptyScrapList
 import co.kr.bemyplan.data.entity.main.scrap.ResponseScrapList
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ScrapListService {
     @GET("v1/plan/bookmark")
-    suspend fun getScrapList(
+    suspend fun fetchDefaultScrapList(
+        @Query("size") size: Int
+    ): ResponseScrapList
+
+    @GET("v1/plan/bookmark")
+    suspend fun fetchQueryScrapList(
         @Query("size") page: Int,
         @Query("sort", encoded = true) sort: String
     ): ResponseScrapList
 
     @GET("v1/post/random")
     suspend fun getEmptyScrapList(): ResponseEmptyScrapList
+
+    @GET("v1/plan/bookmark")
+    suspend fun fetchDefaultMoreScrapList(
+        @Query("size") size: Int,
+        @Query("lastScrapId") lastScrapId: Int
+    ): ResponseScrapList
+
+    @GET("v1/plan/bookmark")
+    suspend fun fetchQueryMoreScrapList(
+        @Query("size") size: Int,
+        @Query("lastScrapId") lastScrapId: Int,
+        @Query("sort", encoded = true) sort: String
+    ): ResponseScrapList
 }

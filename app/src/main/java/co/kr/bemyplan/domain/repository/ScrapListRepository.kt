95747dda26a2961b4339ad3a1ab2a6c998dd3c20
hansh0101 <hansh0101@naver.com>
2022-06-28 17:28:29 +0900

    [FEAT] #323 - 비마이플랜 추천 여행일정 리스트 fetch 구현

--- app/src/main/java/co/kr/bemyplan/domain/repository/ScrapListRepository.kt
+++ app/src/main/java/co/kr/bemyplan/domain/repository/ScrapListRepository.kt
@@ -1,14 +1,11 @@
 package co.kr.bemyplan.domain.repository
 
-import co.kr.bemyplan.data.entity.main.scrap.ResponseEmptyScrapList
 import co.kr.bemyplan.domain.model.list.PlanList
 
 interface ScrapListRepository {
     suspend fun fetchDefaultScrapList(): PlanList
 
     suspend fun fetchQueryScrapList(sort: String): PlanList
-
-    suspend fun fetchEmptyScrapList(): ResponseEmptyScrapList
 
     suspend fun fetchDefaultMoreScrapList(size: Int, lastScrapId: Int): PlanList
 

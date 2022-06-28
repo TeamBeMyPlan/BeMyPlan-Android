6df1dc4755967b57e6072e2fde8633c5658bc4aa
hansh0101 <hansh0101@naver.com>
2022-06-28 17:28:35 +0900

    [FEAT] #323 - 비마이플랜 추천 여행일정 리스트 fetch 구현

--- app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/ScrapListRepositoryImpl.kt
+++ app/src/main/java/co/kr/bemyplan/data/repository/main/scrap/ScrapListRepositoryImpl.kt
@@ -1,11 +1,9 @@
 package co.kr.bemyplan.data.repository.main.scrap
 
 import co.kr.bemyplan.data.api.ScrapListService
-import co.kr.bemyplan.data.entity.main.scrap.ResponseEmptyScrapList
 import co.kr.bemyplan.domain.model.list.PlanList
 import co.kr.bemyplan.domain.repository.ScrapListRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -26,12 +24,6 @@ class ScrapListRepositoryImpl @Inject constructor(
                 10,
                 sort
             ).data
-        }
-    }
-
-    override suspend fun fetchEmptyScrapList(): ResponseEmptyScrapList {
-        return withContext(coroutineDispatcher) {
-            service.getEmptyScrapList()
         }
     }
 
